90c327fdf92ef91f2286dc17882f048519d277c7
Edily Mora <edilyrosa@gmail.com>
2023-12-20 17:16:47 -0400

    test 4

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
 import express from "express";
 import pg from "pg";
-import {config} from 'dotenv';
+import { config } from 'dotenv';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
-config()
+// Cargar variables de entorno desde el archivo .env
+config();
 
+const app = express(); // El servidor
 
-const app = express() //The server
+app.use(bodyParser.json());
+app.use(cors());
 
 const pool = new pg.Pool({
   connectionString: process.env.DB_URL,
-  ssl:true
-}); 
+  ssl: true
+});
 
 app.get('/', (req, res) => {
-    res.send("¡I'm on the home 🚀");
-  });
+  res.send("¡I'm at home 🚀");
+});
 
-app.get('/edily',async  (req, res) => {
-  const result = await pool.query('SELECT NOW()')
-    return res.json(result.rows[0])
-  });
+app.get('/edily', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    return res.json(result.rows[0]);
+  } catch (error) {
+    console.error('Error en la consulta a la base de datos:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000) //will listen on p0rt 3000
-console.log('Server on port 3000');
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
