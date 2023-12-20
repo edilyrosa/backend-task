613aaa7530cbfa53f5344e4c2623599f4d306b7e
Edily Mora <edilyrosa@gmail.com>
2023-12-20 15:30:05 -0400

    firts commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import pg, { Pool } from "pg";
+require('dotenv').config();
+
+const app = express() //The server
+
+const pool = new Pool({
+  connectionString: process.env.DB_URL,
+  ssl:true
+}); 
+
+
+app.get('/edily',async  (req, res) => {
+  const result = await pool.query('SELECT NOW()')
+
+    return res.json(result.rows[0])
+  });
+
+app.get('/', (req, res) => {
+    res.send('¡ESTOY EN EL HOMEeeeeeeeeeeeeeeeee!! 🚀');
+  });
+
+app.listen(3000) //will listen on p0rt 3000
+console.log('Server on port 3000');
