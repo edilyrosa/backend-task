e42f932b9eae68f6a08bfbdfa4d1e147c17ef5db
Edily Mora <edilyrosa@gmail.com>
2023-12-20 16:19:25 -0400

    test 2

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import express from "express";
 import pg from "pg";
 
 import {config} from 'dotenv';
-//require('dotenv').config();
+
+
 
 const app = express() //The server
 
@@ -14,12 +15,11 @@ const pool = new pg.Pool({
 
 app.get('/edily',async  (req, res) => {
   const result = await pool.query('SELECT NOW()')
-
     return res.json(result.rows[0])
   });
 
 app.get('/', (req, res) => {
-    res.send('¡ESTOY EN EL HOMEeeeeeeeeeeeeeeeee!! 🚀');
+    res.send("¡I'm con the home 🚀");
   });
 
 app.listen(3000) //will listen on p0rt 3000
